21dbf2a0cf24bc316ec9830f75d705e7408b5f7c
root <root@dev.airslamit.com>
2017-10-30 04:28:07 -0700

    Skeleton files added

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'Airslamit_DumpOrder',
+        __DIR__
+    );
