04a4325b3fce80ba300b59c66fba818f0a4b3499
Aleksandar Babic <alekski64@icloud.com>
2017-11-01 14:31:50 -0700

    Refactor, few new helper functions.

--- Observer/Sales/Order/SaveAfter.php
+++ Observer/Sales/Order/SaveAfter.php
@@ -7,6 +7,8 @@ class SaveAfter implements ObserverInterface
 {
   protected $_workingDir;
   protected $_ordersDir;
+  protected $_order;
+  protected $_orderFilePath;
 
   /**
    * Constructor will setup working directory and order directory variables, create directory to keep orders CSV files if it does not already exist.
@@ -16,7 +18,7 @@ class SaveAfter implements ObserverInterface
   public function __construct(\Magento\Framework\Filesystem\DirectoryList $workingDir)
   {
     $this->_workingDir = $workingDir;
-    $this->_ordersDir = $this->_workingDir->getPath('var')."/orders/";
+    $this->_ordersDir = $this->_workingDir->getPath("var")."/orders/";
 
     if (!file_exists($this->_ordersDir)) {
       mkdir($this->_ordersDir, 0777, true);
@@ -24,33 +26,63 @@ class SaveAfter implements ObserverInterface
   }
 
   /**
-   * This function will write all headers that are required for Fishbowl order import (more at https://www.fishbowlinventory.com/w/files/csv/importGenericSC.html).
-   * @param type $file 
+   * Will return array of CSV headers that are needed for Fishbowl order import.
+   * More details at https://www.fishbowlinventory.com/w/files/csv/importGenericSC.html
    * @return type
    */
-  public function writeCSVHeaders($file) {
+  private function getCSVHeaders()
+  {
     $headers = "OrderNumber,OrderDate,OrderTax,OrderShipping,OrderHandling,OrderDiscount,OrderNote,OrderShipMethod,OrderCustomerPO,OrderQBClass,OrderLG,ShipFirstName,ShipLastName,ShipAddress1,ShipAddress2,ShipCity,ShipState,ShipZip,ShipCountry,ShipEmail,ShipPhone,ShipFax,BillFirstName,BillLastName,BillAddress1,BillAddress2,BillCity,BillState,BillZip,BillCountry,BillEmail,BillPhone,BillFax,ItemType,ItemNumber,ItemPrice,ItemQuantity,ItemUOM,ItemTaxable,ItemQBClass,ItemNote,KitItem,ShowItem,PaymentProcess,PaymentTotal,Payment Method,PaymentTransactionID,PaymentMisc,PaymentExpirationDate";
-    file_put_contents($file, $headers);
+    return explode(",", $headers);
   }
 
   /**
-   * This function will generate CSV order file that is compataible with Fishbowl order import.
-   * @param type $order 
+   * Will return array of first 11 values for fields that start with Order* 
    * @return type
    */
-  public function generateOutput($order) {
-    $filePath = $this->_ordersDir.$order->getId();
-    $this->writeCSVHeaders($filePath);
+  private function getOrderData()
+  {
+    $dataArray = [];
+    $dataArray['orderId'] = $this->_order->getId();
+    return $dataArray;
   }
 
   /**
-   * This function is observer for event sales_order_save_after
+   * Will write headers and all values to CSV file.
+   * @param type $orderHeaders 
+   * @param type $orderValues 
+   * @return type
+   */
+  private function writeCSV($orderHeaders, $orderValues){
+    $handle = fopen($this->_orderFilePath, "a+");
+    fputcsv($handle, $orderHeaders);
+    fputcsv($handle, $orderValues);
+    fclose($handle);
+  }
+
+  /**
+   * Will get headers, merge all values and call function that will write CSV file.
+   * @return type
+   */
+  private function generateOutput()
+  {
+    $this->_orderFilePath = $this->_ordersDir.$this->_order->getId();
+
+    $orderHeaders = $this->getCSVHeaders();
+    $orderData = $this->getOrderData();
+    $mergedArray = array_merge($orderData);
+
+    $this->writeCSV($orderHeaders,$mergedArray);
+  }
+
+  /**
+   * Will observe event sales_order_save_after.
    * @param \Magento\Framework\Event\Observer $observer 
    * @return type
    */
   public function execute(\Magento\Framework\Event\Observer $observer)
   {
-    $order = $observer->getEvent()->getOrder();
-	  $this->generateOutput($order);
+    $this->_order = $observer->getEvent()->getOrder();
+	  $this->generateOutput();
   }
 }
